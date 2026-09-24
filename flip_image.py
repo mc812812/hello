0f51ada2ed1e12ba695e5c702c7ac3727052d803
mc812812 <3159895494@qq.com>
2026-09-24 21:13:17 +0800

    add python script for image flip

--- flip_image.py
+++ flip_image.py
@@ -1,13 +1,13 @@
 from PIL import Image
- import matplotlib.pyplot as plt
- def flip_image_left_right(img):
+import matplotlib.pyplot as plt
+def flip_image_left_right(img):
      """
      输入图片对象，返回左右翻转后的图片
      """
      flipped_img = img.transpose(Image.FLIP_LEFT_RIGHT)
      return flipped_img
- if __name__ == "__main__":
-     # 测试图片，放到hello文件夹里，名字改成test.jpg
+if __name__ == "__main__":
+     # 测试图片，放到当前文件夹里，名字改成test.jpg
      original = Image.open("test.jpg")
      flipped = flip_image_left_right(original)
      # 并排显示原图 和 翻转图
